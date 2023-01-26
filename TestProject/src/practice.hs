54ddab537c6da14dad2b609b54523613e4f9dfad
AmnaRiaz <amanamana606@gmail.com>
2023-01-26 09:31:47 +0500

    haskell

--- TestProject/src/practice.hs
+++ TestProject/src/practice.hs
@@ -1,11 +1,34 @@
 module Lib
     ( 
-    someFunc
+        someFunc
+       -- doubleUs
     ) where
 
 
---returns the string
+-- --returns the string
 someFunc :: IO ()
 someFunc = putStrLn "Amn is learning haskell"
+
+
+
+doubleUs :: Num a => a -> a -> a
+doubleUs x y = x*5 + y*5
+
+double :: Num a => a -> a
+double x = x + x
+
+quadruple :: Num a => a -> a
+quadruple x = double (double x)
+
+average :: Foldable t => t Int -> Int
+average ns = sum ns `div` length ns
+
+factorial :: (Num a, Enum a) => a -> a
+factorial n = product [1..n]
+
+
+
+ 
+
 
 
