890dbd9c81901a66393139734db3c64d2d6cb2fd
AmnaRiaz <amanamana606@gmail.com>
2023-01-26 10:48:27 +0500

    haskell

--- TestProject/src/practice.hs
+++ TestProject/src/practice.hs
@@ -9,7 +9,7 @@ module Lib
 someFunc :: IO ()
 someFunc = putStrLn "Amn is learning haskell"
 
-
+--Simple Functions
 
 doubleUs :: Num a => a -> a -> a
 doubleUs x y = x*5 + y*5
@@ -26,9 +26,33 @@ average ns = sum ns `div` length ns
 factorial :: (Num a, Enum a) => a -> a
 factorial n = product [1..n]
 
+--If else stateement 
+checkAge :: Int -> String 
+checkAge age = 
+    if age == 18
+        then "She is 18"
+        else "She is not 18"
 
 
- 
+--Guards in place of If else        
+checkTheDay :: String -> String 
+checkTheDay day
+    | day == "M" = "Its Monday "
+    | day == "T" = "Its Tuesday"
+    | day == "W" = "Its wednesday"
+    | day == "TH" = "Its Thursday"
+    | day == "TF" = "Its friday"
+    | otherwise = "nothing"
+       
+
+
+-- let expression 
+amn :: Int -> Int
+amn x =   let 
+    x = 5
+    y = x + 10
+    z = y + 20
+    in z
 
 
 
